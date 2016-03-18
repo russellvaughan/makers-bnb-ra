8f1f910548a305ff8288a35f4920c2bbdb63af98
Mark Hill <futonfutonda@gmail.com>
2016-03-18 11:41:58 +0000

    connected frontend with backend

--- frontend/app/create/forms.js
+++ frontend/app/create/forms.js
@@ -7,9 +7,29 @@ angular.module('makersbnb.createspace', ['ngRoute'])
   });
 }])
 
-.controller('CreateSpaceCtrl', ["$scope", function($scope) {
+.controller('CreateSpaceCtrl', ["$scope", "$http", "$location", function($scope, $http, $location) {
   $scope.createSpace = function(field){
-    $scope.newSpace = field
-    return $scope.newSpace
+    var param = JSON.stringify({name: "bob"});
+    $http.post("http://localhost:3000/spaces.json",
+  param ).success(function(data, status){
+    console.log(data);
+  });
+    // $scope.newSpace = field
+    // return $scope.newSpace
   };
 }]);
+
+// restauranteur.controller 'RestaurantIndexCtrl', ['$scope', '$location', '$http', ($scope, $location, $http) ->
+// $scope.restaurants = []
+//   $http.get('./restaurants.json').success((data) ->
+//     $scope.restaurants = data
+//   )
+//   $scope.addRestaurant = (test) ->
+//     $http({
+//       url: '/restaurants#create',
+//       method: "POST",
+//       data: JSON.stringify({name:test}),
+//       headers: {'Content-Type': 'application/json'}
+//     })
+//
+// ]
--- makersBnB/config/routes.rb
+++ makersBnB/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :spaces
   resources :spaces, only: [:index]
+  match '*any' => 'application#options', :via => [:options]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
