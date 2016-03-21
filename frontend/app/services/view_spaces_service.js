35170b1d7470e3fc1438fa6522b95524d39917d5
russellvaughan <russellvaughan@gmail.com>
2016-03-21 09:36:54 +0000

    merged with origin master

--- frontend/app/services/view_spaces_service.js
+++ frontend/app/services/view_spaces_service.js
@@ -1,12 +1,9 @@
-
 angular.module('makersbnb.viewspaces')
-
 .service('ViewSpaceService', function($http) {
    return {
      getSpace: function() {
        return $http.get('http://localhost:3000/spaces.json').then(function(result) {
            return result.data;
-
 });
 }  
 }       
